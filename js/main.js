c797f267bc761e7cfda487fa461813c9087abf61
KTamas <ktamas@ktamas.com>
2013-11-09 12:32:29 +0100

    Reformatting; initial work on stops

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 var apikey = "1b71620f-a0f1-4448-afe9-069e40b1ef51";
 var base = "http://api.vasttrafik.se/bin/rest.exe/v1/";
 
-function gDate(dd, tt) {
+var gDate = function (dd, tt) {
   var d = dd.split('-');
   var t = tt.split(':');
   return new Date(d[0],d[1],d[2],t[0],t[1]);
 }
 
-timedecorator = function (params) {
+var timedecorator = function (params) {
   var dt = gDate(this.rtDate, this.rtTime);
   to_return = (dt - base) / 1000 / 60;
   return to_return === 0 ? "Nu" : to_return;
 };
-      
-directives = {
+
+var directives = {
   DepartureBoard : {
     Departure : {
       diff : { text: timedecorator }
@@ -34,21 +34,22 @@ navigator.geolocation.getCurrentPosition(function(position) {
     },
     dataType: "jsonp"
   }).done(function(data) {
-     var first = data.LocationList.StopLocation[0].id;
-    $.ajax({
-      url: base + "departureBoard?jsonpCallback=?",
-      data: {
-        "authKey": apikey,
-        "format": "json",
-        "id": first
-      },
-      dataType: "jsonp",
-      cache: false
-    }).done(function(data) {
-       t = data.DepartureBoard.servertime;
-       d = data.DepartureBoard.serverdate;
-       window.base = gDate(d, t);
-       Transparency.render( document.getElementById("departure"), data, directives);
-    });
-  });
+   Transparency.render(document.getElementById("stops"), data);
+   var first = data.LocationList.StopLocation[0].id;
+   $.ajax({
+    url: base + "departureBoard?jsonpCallback=?",
+    data: {
+      "authKey": apikey,
+      "format": "json",
+      "id": first
+    },
+    dataType: "jsonp",
+    cache: false
+  }).done(function(data) {
+   t = data.DepartureBoard.servertime;
+   d = data.DepartureBoard.serverdate;
+   window.base = gDate(d, t);
+   Transparency.render( document.getElementById("departure"), data, directives);
+ });
+});
 });
